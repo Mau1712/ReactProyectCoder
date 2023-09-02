ff17e2dec19fdf5e839dda72f557665ceda81606
Mau1712 <109633882+Mau1712@users.noreply.github.com>
2023-09-02 00:06:05 -0300

    BugRepairItems

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -7,32 +7,39 @@ import { useCart } from '../../Context/CartContext';
 
 const ItemDetail = ({ product }) => {
 
-const { addToCart } = useCart(); // Obtén la función addToCart desde el contexto
+  const { addToCart } = useCart(); // Obtén la función addToCart desde el contexto
 
   const onAddProduct = () => {
     addToCart(product); // Agrega el producto al carrito
   }
 
-    return (
+  return (
+    <>
+      {product && product.Titulo ? (
         <Container className='item-detail-container itemDetailCard'>
-            {/* <Card.Img className='imgDetailCard'  variant="top" src={product.imagen} /> */}
-            <Card className="text-center cardDetails">
-                <Card.Header>Detalles del Producto</Card.Header>
-                
-                <Card.Body>
-                    <Card.Title>{product.Titulo}</Card.Title>
-                    <Card.Text>{product.Detalle}</Card.Text>
-                    <Card.Text>Aromas: {product.Aromas}</Card.Text>
-                    <Card.Text>Tipo: {product.categoria}</Card.Text>
-                    <Card.Text>Precio: {product.Precio.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</Card.Text>
-                    <Button className='addCartDetailItemButton' variant="primary" onClick={onAddProduct}>Agregar a la Bolsa</Button>
-                </Card.Body>
-                <Card.Footer className="text-muted">La oferta termina en 2 dias</Card.Footer>
-            </Card>
+          {/* <Card.Img className='imgDetailCard'  variant="top" src={product.imagen} /> */}
+          <Card className="text-center cardDetails">
+            <Card.Header>Detalles del Producto</Card.Header>
+
+            <Card.Body>
+              <Card.Title>{product.Titulo}</Card.Title>
+              <Card.Text>{product.Detalle}</Card.Text>
+              <Card.Text>Aromas: {product.Aromas}</Card.Text>
+              <Card.Text>Tipo: {product.categoria}</Card.Text>
+              <Card.Text>Precio: {product.Precio.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</Card.Text>
+              <Button className='addCartDetailItemButton' variant="primary" onClick={onAddProduct}>Agregar a la Bolsa</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">La oferta termina en 2 dias</Card.Footer>
+          </Card>
         </Container>
-    );
+      ) : (
+        <p>No se encontró el producto</p>
+      )}
+    </>
+  );
 };
 
 export default ItemDetail;
+
 
 {/*  */}
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,36 +3,46 @@ import { useParams } from 'react-router-dom';
 import './ItemDetailsContainer.css';
 import Loader from '../Loader';
 import ItemDetail from './ItemDetail';
+import { fireStore } from '../../firebase/firebase'; 
+import { collection, query, where, getDocs } from 'firebase/firestore';
 
 const ItemDetailContainer = () => {
+    
     const [product, setProduct] = useState(null);
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
 
-
     useEffect(() => {
-        setTimeout(() => {
-            fetch('/productsBD.json')
-                .then((res) => res.json())
-                .then((data) => {
-                    console.log('Product Data:', data);
-                    const selectedProduct = data.find(item => item.id === id);
-                    console.log('Selected Product:', selectedProduct);
-                    setProduct(selectedProduct);
-                    setLoading(false);
-                })
-                .catch((error) => console.error(error))
-                .finally(() => setLoading(false));
-        }, 2000);
+        setLoading(true);
+
+        const fetchProduct = async () => {
+          const q = query(collection(fireStore, 'Item'), where("id", "==", id));
+          
+          const querySnapshot = await getDocs(q);
+          
+
+          if (!querySnapshot.empty) {
+            const docSnap = querySnapshot.docs[0];
+            setProduct(docSnap.data());
+            console.log("Valor del objeto:", docSnap.data());
+          } else {
+            console.log('No se encontró el documento en la base de datos.');
+          }
+
+
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+
+        };
+
+        fetchProduct();
     }, [id]);
 
     return (
         <>
          {loading ? <Loader /> : <ItemDetail product={product} /> }        
         </>
-        
-           
-      
     );
 };
 
